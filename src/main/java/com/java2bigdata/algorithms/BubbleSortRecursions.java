6d9582a67e512eb5f41427fe51bdbd23f302b383
emahendhar <mmahender66@gmail.com>
2024-01-18 20:29:25 +0530

    updated bubblesort

--- src/main/java/com/java2bigdata/algorithms/BubbleSortRecursions.java
+++ src/main/java/com/java2bigdata/algorithms/BubbleSortRecursions.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BubbleSortRecursions {
     public static void main(String[] args) {
 
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
 
         bubbleSort(arr,arr.length);
         System.out.println(Arrays.toString(arr));
@@ -19,20 +19,21 @@ public class BubbleSortRecursions {
 
         int count=0;
 
-        for(int i=0;i<n-1;i++){
-            if(a[i]>a[i+1]) {
+        for(int i=0;i<n-1;i++) {
+            if (a[i] > a[i + 1]) {
                 //swap
                 int temp = a[i];
                 a[i] = a[i + 1];
                 a[i + 1] = temp;
                 count = count + 1;
             }
+        }
 
             if(count==0)
                 return;
 
             bubbleSort(a,n-1);
-        }
+
     }
 
 }
