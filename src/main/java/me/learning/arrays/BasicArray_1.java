331aa1293e9c251a381420f0460831e120697a6a
emahendhar <mmahender66@gmail.com>
2025-04-04 08:55:15 +0530

    updated commit

---
+++ src/main/java/me/learning/arrays/BasicArray_1.java
@@ -0,0 +1,27 @@
+package me.learning.arrays;
+
+import java.util.List;
+
+public class BasicArray_1 {
+
+    public static void main(String[] args) {
+
+        //array initialization and declaration
+
+
+        int a1[];
+        int a2[]={1,2,3,4,5,6,7};
+        String[] a3=new String[]{"one","two"};
+        char[] a4= new char[3];     //default values 0 for int and space for char
+        a4[1]='a';
+        a4[0]='r';
+        a4[2]=3;
+
+
+
+
+
+    }
+
+
+}
